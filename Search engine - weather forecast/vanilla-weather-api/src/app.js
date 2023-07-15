0b1f32ec90c0f68cc735fce095e31040512429bf
Alyona4433 <alena4433@gmail.com>
2023-07-15 19:02:49 +0200

    Added forecast.HTML

--- Search engine - weather forecast/vanilla-weather-api/src/app.js
+++ Search engine - weather forecast/vanilla-weather-api/src/app.js
@@ -24,7 +24,28 @@ function formatDate(timestamp) {
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = "Forecast";
+
+  let forecastHTML = "Hello";
+  forecastHTML =
+    forecastHTML +
+    `
+
+  <div class="row">
+    <div class="col-2">
+      <div class="weather-forecast-date">Friday</div>
+      <img
+        src="https://openweathermap.org/img/wn/04d@2x.png"
+        alt=""
+        width="36"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">18°</span>
+        <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+    </div>
+  </div>;
+`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
